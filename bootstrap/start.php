a2b69030a642e655d4141e22a01af4a968c1becf
Rick Bennett <RBennett1106@Gmail.com>
2014-05-10 15:17:34 -0400

    added production environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('MININT-9BESIED'),
-	'staging' => array('www.rickbennettcoding.com'),
+	'production' => array('rickbennettcoding.com'),
 
 ));
 
