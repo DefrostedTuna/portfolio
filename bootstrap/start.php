ecc8ff7b38e2c1a8b8fabc23da2f00fa3a317b00
Rick Bennett <RBennett1106@Gmail.com>
2014-05-10 15:08:06 -0400

    changed environment to staging

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('MININT-9BESIED'),
+	'staging' => array('www.rickbennettcoding.com'),
 
 ));
 
